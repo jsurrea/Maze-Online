25a9a57df3f86ddc3e755dc27203793a247c3fba
Juan Sebastián Urrea <js.urrea@uniandes.edu.co>
2022-12-16 00:53:27 -0500

    Player moving

--- game.js
+++ game.js
@@ -1,39 +1,146 @@
-const canvas = document.querySelector('#game')
-const gameContext = canvas.getContext('2d')
+// Canvas DOM variables
+const canvas = document.querySelector('#game');
+const gameContext = canvas.getContext('2d');
 
-function setCanvasSize() {
+// Button DOM variables
+const buttonUp = document.querySelector('#up');
+const buttonLeft = document.querySelector('#left');
+const buttonRight = document.querySelector('#right');
+const buttonDown = document.querySelector('#down');
 
-    let canvasSize = Math.min(window.innerHeight, window.innerWidth) * 0.75;
+// View variables
+let canvasSize;
+let elementSize;
 
+// Game variables
+let map2DArray;
+let playerPosition;
+
+function loadMap(level) {
+
+    // Set current map to display
+    currentMap = maps[level];
+
+    // Parse the config String
+    return currentMap.split("\n")
+
+    // Clean unwanted whitespaces
+    .map(element => element.trim())
+
+    // Remove unused elements
+    .filter(element => !!element.length)
+
+    // Get each item
+    .map(row => row.split(""));
+}
+
+function loadPlayer() {
+
+    // Find where is the player
+    let rowContainsPlayer = map2DArray.map(row => row.indexOf('O'));
+
+    return {
+        posX: rowContainsPlayer.find(indexOf => indexOf != -1),
+        posY: rowContainsPlayer.findIndex(indexOf => indexOf != -1),
+    };
+}
+
+function renderCanvas() {
+
+    // Make it responsive
+    canvasSize = Math.min(window.innerHeight, window.innerWidth) * 0.75;
+    elementSize = canvasSize / 10;   
+    
+    // Set a square
     canvas.setAttribute('width', canvasSize);
     canvas.setAttribute('height', canvasSize);
 
-    let elementSize = canvasSize / 10;   
+    // Set styles
+    gameContext.font = elementSize + 'px Verdana';
+    gameContext.textAlign = 'end';
 
-    gameContext.font = elementSize + 'px Verdana'
-    gameContext.textAlign = 'end'
+    // Make a 10x10 grid
+    map2DArray.forEach((row, y) => row.forEach((item, x) => {
 
-    currentLevel = 2 // 🌳
-    currentMap = maps[currentLevel]
-    currentRows = currentMap.split("\n")
-        .map(element => element.trim())
-        .filter(element => !!element.length)
+        // Current item
+        let emoji = emojis[item];
+        let posX = elementSize * (x + 1);
+        let posY = elementSize * (y + 1);
 
-    for(let x = 1; x <= 10; x++) {
-        currentRow = currentRows[x-1]
-        currentItems = currentRow.split("")
-        for(let y = 1; y <= 10; y++) {
-            currentItem = currentItems[y-1]
-            gameContext.fillText(emojis[currentItem], elementSize * x, elementSize * y)
-        }
-    }
+        // Fill map
+        gameContext.fillText(emoji, posX, posY);
+    }));
+
+    // Set player
+    movePlayer();
 }
 
 function startGame() {
 
-    setCanvasSize();
+    map2DArray = loadMap(2);
+    playerPosition = loadPlayer();
+    renderCanvas();
 
 }
 
-window.addEventListener('load', startGame)
-window.addEventListener('resize', setCanvasSize)
+function movePlayer() {
+    // Set variables
+    let emoji = emojis['PLAYER'];
+    let posX = elementSize * (playerPosition.posX + 1);
+    let posY = elementSize * (playerPosition.posY + 1);
+
+    // Render on Canvas
+    gameContext.fillText(emoji, posX, posY);
+}
+
+function moveUp() {
+    playerPosition.posY -= 1;
+    renderCanvas()
+}
+
+function moveLeft() {
+    playerPosition.posX -= 1;
+    renderCanvas()
+}
+
+function moveRight() {
+    playerPosition.posX += 1;
+    renderCanvas()
+}
+
+function moveDown() {
+    playerPosition.posY += 1;
+    renderCanvas()
+}
+
+function moveByKeys(event) {
+
+    // Control keyboard movement
+    switch(event.key) {
+        case 'ArrowUp':
+            moveUp();
+            break;
+        case 'ArrowLeft':
+            moveLeft();
+            break;
+        case 'ArrowRight':
+            moveRight();
+            break;
+        case 'ArrowDown':
+            moveDown();
+            break;
+    }
+
+    //renderCanvas()
+}
+
+// General event listeners
+window.addEventListener('load', startGame);
+window.addEventListener('resize', renderCanvas);
+window.addEventListener('keydown', moveByKeys);
+
+// Keydown event listeners
+buttonUp.addEventListener('click', moveUp);
+buttonLeft.addEventListener('click', moveLeft);
+buttonRight.addEventListener('click', moveRight);
+buttonDown.addEventListener('click', moveDown);
