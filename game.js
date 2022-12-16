9d50a7f287900e913ac9f278e6f8629a2abd234d
Juan Sebastian Urrea Lopez <jurrea49@hotmail.com>
2022-12-15 22:11:26 -0500

    Fill map

--- game.js
+++ game.js
@@ -1,14 +1,30 @@
 const canvas = document.querySelector('#game')
 const gameContext = canvas.getContext('2d')
 
-function startGame() {
-    gameContext.fillRect(50,0,100,100)
-    gameContext.clearRect(50,50,50,50)
+function setCanvasSize() {
 
-    gameContext.font = '25px Verdana'
-    gameContext.fillStyle = 'purple'
-    gameContext.textAlign = 'start'
-    gameContext.fillText('Platzi', 50, 50)
+    let canvasSize = Math.min(window.innerHeight, window.innerWidth) * 0.75;
+
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+
+    let elementSize = canvasSize / 10;   
+
+    gameContext.font = elementSize + 'px Verdana'
+    gameContext.textAlign = 'end'
+
+    for(let x = 1; x <= 10; x++) {
+        for(let y = 1; y <= 10; y++) {
+            gameContext.fillText('🌳', elementSize * x, elementSize * y)
+        }
+    }
+}
+
+function startGame() {
+
+    setCanvasSize();
+
 }
 
 window.addEventListener('load', startGame)
+window.addEventListener('resize', setCanvasSize)
